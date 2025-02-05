13c7b363be3adad57e9b6132293763eaeba6331f
Agaba Andre <agabaandre@gmail.com>
2025-02-05 16:09:57 +0300

    searches

--- resources/views/home/partials/top_searches.blade.php
+++ resources/views/home/partials/top_searches.blade.php
@@ -102,7 +102,7 @@
                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                    <div class="position-relative text-center">
                        <a id="explore" href="{{ url('records') }}"
-                           class="btn btn-md theme-bg rounded text-light hover-theme">Explore More Resources<i
+                           class="btn btn-md theme-bg rounded text-light hover-theme">More<i
                                class="lni lni-arrow-right-circle ml-2"></i></a>
                    </div>
                </div>
