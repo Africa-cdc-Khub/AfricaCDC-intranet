f6a4cd6df4f4b49af5f051308cb645228ee5438e
Agaba Andre <agabaandre@gmail.com>
2025-02-05 16:34:07 +0300

    searches

--- resources/views/publications/search.blade.php
+++ resources/views/publications/search.blade.php
@@ -13,7 +13,7 @@
                         <h4 class="ml-3">Search Results</h4>
                     </div>
 
-                    @include('partials.quiz.quiz')
+                    {{-- @include('partials.quiz.quiz') --}}
 
                     <div class="row text-success ml-2 mb-2">
                         {{ isset($_GET['tag']) ? ' Tag: ' . $_GET['tag'] : '' }}
