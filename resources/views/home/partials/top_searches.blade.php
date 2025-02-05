5e2e35c7b47c794ac6b3ad58e33c075cc2ce7aeb
Agaba Andre <agabaandre@gmail.com>
2025-02-05 15:45:47 +0300

    intranet changes

--- resources/views/home/partials/top_searches.blade.php
+++ resources/views/home/partials/top_searches.blade.php
@@ -5,7 +5,7 @@
            <div class="row justify-content-center" data-aos="fade-in">
                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                    <div class="sec_title position-relative text-center mb-5">
-                       <h2 class="ft-bold">Top Searches</span></h2>
+                       <h2 class="ft-bold">Africa CDC Systems</span></h2>
                    </div>
                </div>
            </div>
